296cd05f13258db36d12ad2b286053fef4ec950b
Simona Domnisoru <simona.domnisoru@gmail.com>
2018-02-23 14:43:06 +0100

    Move code to react compoenent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 
-
 class App extends Component {
-  render() {
-    return (
-      <div>
-          Hello world
+
+    renderMap = () => {
+        var google = window.google;
+        google.charts.load('current', {
+            'packages': ['geochart'],
+            'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+        })
+
+        google.charts.setOnLoadCallback(drawRegionsMap);
+
+        function drawRegionsMap() {
+            var data = google.visualization.arrayToDataTable([
+                ['Country', 'Popularity'],
+                ['Germany', 200],
+                ['United States', 300],
+                ['Brazil', 400],
+                ['Canada', 500],
+                ['France', 600],
+                ['RU', 700]
+            ]);
+
+            var options = {
+                colorAxis: { colors: ['red', 'orange', 'green'] },
+                enableRegionInteractivity: true
+            };
+
+            var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+            chart.draw(data, options);
+        }
+    }
+
+    render() {
+        this.renderMap();
+        return (
+            <div>
+                Hello world
       </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
